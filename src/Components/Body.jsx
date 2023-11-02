47e1700ebe2e976683313354f5c36a623c20a7ba
Arucole <96977950+Arucole@users.noreply.github.com>
2023-11-02 23:55:57 +0100

    updates

--- src/Components/Body.jsx
+++ src/Components/Body.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Body.css";
 import imageProduct from "../images/imageProduct.jpg";
 import imageProduct2 from "../images/imageProduct2.jpg";
@@ -34,26 +34,42 @@ function Body() {
     },
   ];
 
+  const [slide, setSlide] = useState(0);
+  const [number, setNumber] = useState(0);
+
+  const nextSlide = () => {
+    setSlide(slide === displayImages.length - 1 ? 0 : slide + 1);
+  };
+
+  const prevSlide = () => {
+    setSlide(slide === 0 ? displayImages.length - 1 : slide - 1);
+  };
+
   return (
     <div className="bodyContainer">
       <div className="carousel">
-        <BsArrowLeftCircle className="arrow arrow-left" />
+        <BsArrowLeftCircle className="arrow arrow-left" onClick={prevSlide} />
         {displayImages.map((item, id) => (
           <img
             src={item.image}
             alt="Product Image"
             key="id"
-            className="slide"
+            className={slide === id ? "slide" : "slide slide-hidden"}
           />
         ))}
-        <BsArrowRightCircle className="arrow arrow-right" />
+        <BsArrowRightCircle className="arrow arrow-right" onClick={nextSlide} />
         <span className="indicatorImage">
           {displayImages.map((item, id) => (
             <img
               src={item.thumbnail}
               alt="Product Image"
+              onClick={() => setSlide(id)}
               key="id"
-              className="thumbnailImage"
+              className={
+                slide === id
+                  ? "thumbnailImage"
+                  : "thumbnailImage thumbnail-inactive"
+              }
             />
           ))}
         </span>
@@ -62,23 +78,31 @@ function Body() {
       <div className="cartDetail">
         <h3>sneaker company</h3>
         <h2>Fall Limited Edition Sneakers</h2>
-        <p>
+        <p className="productDetail">
           These low-profile sneakers are your perfect casual wear companion.
           Feauturing a durable rubber outer sole, they'll withstand everything
           the weather can offer.
         </p>
-        <div>
-          <p>$125.00</p>
-          <p>50%</p>
-        </div>
-        <p>$250.00</p>
-        <div>
-          <div>
-            <button>-</button>
-            <p>0</p>
-            <button>+</button>
+        <div className="priceDetail">
+          <p className="price">$125.00</p>
+          <div className="discounttag">
+            <p className="discount">50%</p>
           </div>
-          <button>Add To Cart</button>
+        </div>
+        <p className="priceLine">$250.00</p>
+        <div className="regulation">
+          <div className="keyReg">
+            <p className="regBtn">-</p>
+            <p>0</p>
+            <p className="regBtn">+</p>
+          </div>
+          <div className="cartBtn">
+            <div className="Btn">
+              {" "}
+              <BsCart3 />
+              Add To Cart
+            </div>
+          </div>
         </div>
       </div>
     </div>
